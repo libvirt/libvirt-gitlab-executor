d9514e05ac54d332c2ba3019ec47ebc334c0f6e4
Erik Skultety <eskultet@redhat.com>
2021-11-09 14:21:10 +0100

    Remove the Shell versions of the scripts
    Previous patch rewrote the Bash scripts to Python, so we don't need the Bash
    scripts anymore.
    
    Signed-off-by Erik Skultety <eskultet@redhat.com>

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "./job${CUSTOM_ENV_CI_JOB_ID}"
-
-set -eo pipefail
-
-# destroy the transient VM and the overlay storage
-virsh destroy "${VM_NAME}"
-virsh vol-delete --pool default "${VM_IMAGE}"
