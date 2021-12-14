ed77cec35257a1240ae3bc166d2090bec2d275b5
Erik Skultety <eskultet@redhat.com>
2021-12-14 13:55:26 +0100

    cloud-init: Introduce a module wrapping cloud-init setup
    The module represents various cloud-init related setup steps.
    As part of the module introduction is a definition of basic HTTP server
    whose only purpose is to listen for a POST request coming from the
    machine instance's cloud-init phone home module notifying us of the
    machine's network availability.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

---
+++ src/provisioner/cloud_init.py
@@ -0,0 +1,107 @@
+# cloud_init.py - module wrapping basic cloud init setup
+#
+# Copyright (C) 2021 Red Hat, Inc.
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import logging
+import socket
+import threading
+import urllib.parse
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+log = logging.getLogger(__name__)
+
+
+class _MyHTTPHandler(BaseHTTPRequestHandler):
+    """Handles HTTP requests to the phone home server."""
+
+    def do_POST(self):
+        if not self.path.strip("/") == "nocloud":
+            self.send_response(404)
+        else:
+            content_length = int(self.headers.get("content-length"))
+            post_query_string = self.rfile.read(content_length).decode("utf-8")
+            post_data = urllib.parse.parse_qs(post_query_string)
+
+            if self.server.instance_hostname != post_data["hostname"][0]:
+                self.send_response(403)
+            else:
+                self.server.instance_phoned_home = True
+                self.send_response(200)
+        self.end_headers()
+
+    def log_message(self, format, *args, **kwargs):
+        # disable the default request handler logging
+        pass
+
+
+class _MyHTTPServer(HTTPServer):
+    def __init__(self, instance_hostname, address):
+        self.instance_hostname = instance_hostname
+        self.instance_phoned_home = False
+        super().__init__(address, _MyHTTPHandler)
+
+    def run(self):
+        while not self.instance_phoned_home:
+            self.handle_request()
+
+
+class CloudInitPhoneHomeServer():
+    """
+    Implements an asynchronous HTTP cloud-init phone home server.
+
+    Waits for a cloud-init powered instance to use the phone home module.
+    This class provides an interface where the HTTP server runs asynchronously.
+    In order to check whether the instance has already phoned home, the caller
+    needs to use the 'wait()' method which blocks until the condition
+    (the instance phoned home) was met.
+    """
+
+    @property
+    def address(self):
+        return self._httpd.server_address
+
+    def __init__(self, instance_hostname, address=None):
+        """Initialize the server.
+        :param address: follows the AF_INET family address specification -->
+                        (host, port) tuple; if no address was specified, both
+                        host IP and port the server will listen on will be
+                        selected automatically
+        :param instance_id: identification of the instance that is expected
+                            to call back via cloud-init
+        """
+
+        def server_run(httpd):
+            log.debug("Server thread created, running HTTP server on "
+                      f"{httpd.server_name}:{httpd.server_port}")
+
+            with httpd:
+                httpd.run()
+
+        log.debug("Creating HTTP phone home server")
+
+        if address is None:
+            # port 0 means the OS will pick the first available port
+            address = (socket.gethostbyname(socket.getfqdn()), 0)
+
+        self._httpd = _MyHTTPServer(instance_hostname, address)
+        self._httpd_thread = threading.Thread(target=server_run,
+                                              args=(self._httpd, ),
+                                              daemon=True)
+        self._httpd_thread.start()
+        self._address = None
+        self._instance = instance_hostname
+
+    def wait(self):
+        """Synchronous blocking wait until the machine instance phones home."""
+
+        timeout = 120
+
+        log.debug(f"Waiting for '{self._instance}' to phone home ({timeout}s)")
+        self._httpd_thread.join(timeout=timeout)
+
+        if self._httpd_thread.is_alive():
+            raise Exception(f"Waiting for instance timed out ({timeout}s)")
