0e084eb87d5bb68e4b9a27489af111ba9ddef0f1
Erik Skultety <eskultet@redhat.com>
2021-12-14 14:00:13 +0100

    cloud_init: Introduce a helper to construct cloud-init user data YAML
    An instance will be freshly initialized for each GitLab CI pipeline
    job. Virt-install does support the 'NoCloud' cloud-init data source
    which means that the only thing left for us to do is to provide the
    user-data cloud config and this helper does exactly that.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- src/provisioner/cloud_init.py
+++ src/provisioner/cloud_init.py
@@ -11,8 +11,41 @@ import urllib.parse
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+from provisioner.ssh import get_user_ssh_keys
+
 
 log = logging.getLogger(__name__)
+
+
+def get_user_data(hostname, phone_home_address):
+
+    gitlab_runner = {
+        "name": "gitlab-runner",
+        "sudo": "ALL=(ALL) NOPASSWD:ALL",
+        "ssh_authorized_keys": get_user_ssh_keys(),
+    }
+
+    chpasswd = {
+        "list": ["root:RANDOM", "gitlab-runner:RANDOM"],
+        "expire": False,
+    }
+
+    host = phone_home_address[0]
+    port = phone_home_address[1]
+    phone_home = {
+        "url": f"http://{host}:{port}/$INSTANCE_ID",
+        "post": "all",
+    }
+
+    user_data = {}
+    user_data["users"] = [gitlab_runner]
+    user_data["chpasswd"] = chpasswd
+    user_data["fqdn"] = hostname
+    user_data["phone_home"] = phone_home
+
+    log.debug(f"Cloud-init user data: {user_data}")
+
+    return user_data
 
 
 class _MyHTTPHandler(BaseHTTPRequestHandler):
