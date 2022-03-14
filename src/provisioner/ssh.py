libvirt/libvirt-gitlab-executor

commit 8c9cea41f0c870f369c259cff769edf0d0cf3f98

ssh: Return the remote command exit status instead of raising exception
The exception on its own doesn't really add that much value to the
already informative output. If anything, the command's exit status
should be returned from the 'exec' method.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

src/provisioner/ssh.py
@@ -95,5 +95,4 @@ class SSHConn:
             print(data.decode(), end="", flush=True)
             data = channel.recv(1024)
 
-        if channel.recv_exit_status() != 0:
-            raise Exception(f"Command execution failed ({cmd_str})")
+        return channel.recv_exit_status()

commit 55359564709cada801ffb874c156fc71d3096059

provisioner: ssh: Negate the returned error code from SSH command exec
The exit status comes from bash so we need to transform it
appropriately to standard programming conventions.

Fixes: 8c9cea41f0c870f369c259cff769edf0d0cf3f98
Signed-off-by: Erik Skultety <eskultet@redhat.com>

src/provisioner/ssh.py
@@ -100,4 +100,5 @@ class SSHConn:
             print(data.decode(), end="", flush=True)
             data = channel.recv(1024)
 
-        return channel.recv_exit_status()
+        rc = channel.recv_exit_status()
+        return -rc