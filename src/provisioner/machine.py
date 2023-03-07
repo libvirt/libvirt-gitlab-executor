libvirt/libvirt-gitlab-executor

commit 96ca15f59ddb317b5499d126311283c53022721e

machine: Improve handling of SSH's "Name or service not known"
Depending on Paramiko library version and libvirt dnsmasq's leases file
trying to SSH to a fresh machine during its boot sequence may lead to 2
different types of exception signaling that the network is not up
yet and we have to wait:
- OSError with errno -2 coming from Python's socket library
- NoValidConnectionsError with errno 113 coming from Paramiko itself

Paramiko states in their docs that NoValidConnectionsError is a wrapper
exception type that wraps *all* connection related errors which is not
true when it comes to calling 'getaddrinfo' to resolve a hostname
where they only implement a "fail fast" mechanism and so
socket.gaierror instead of NoValidConnectionsError is raised.
Unfortunately for us this means we have to handle both cases as we
have essentially no control over which one will be thrown at a given
time.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

src/provisioner/machine.py
@@ -87,17 +87,22 @@ class Machine:
             try:
                 self.connect(ssh_key_path)
                 return
-            except ssh_exception.NoValidConnectionsError as ex:
-                # NoValidConnectionsError is a subclass of various socket
-                # errors which in turn is a subclass of OSError. We're
-                # specifically interested in errno 113 'No route to host'
-                # which we can ignore for the duration of the timeout
-                # period
-                for error in ex.errors.values():
-                    if isinstance(error, OSError) and error.errno == 113:
-                        break
-                else:
+            except (OSError, ssh_exception.NoValidConnectionsError) as ex:
+                if isinstance(ex, OSError):
+                    if ex.errno == -2:  # Name or service not known
+                        continue
                     raise ex
+                else:
+                    # NoValidConnectionsError is a subclass of various socket
+                    # errors which in turn is a subclass of OSError. We're
+                    # specifically interested in errno 113 'No route to host'
+                    # which we can ignore for the duration of the timeout
+                    # period
+                    for error in ex.errors.values():
+                        if isinstance(error, OSError) and error.errno == 113:
+                            break
+                    else:
+                        raise ex
 
         raise Exception(f"Failed to connect to {self.name}: timeout reached")
 

commit 7867fd1c059e75b78f2cb98804b535d0f27bbd31

machine: provision: Split the combined exception handler in two
The problem with combining paramiko's NoValidConnectionsError and
generic OSError in a single handler as commit 96ca15f5 did is that
NoValidConnectionsError is a subclass of OSError and hence the
EAI_NONAME errno check is performed against it. The problem is that
NoValidConnectionsError is structured differently than a plain OSError
in that it doesn't have a top-level 'errno' attribute. For the reason
above we also cannot define the handler for OSError as the first one
because the 'except' clauses are evaluated in order.

Fixes: 96ca15f5

Signed-off-by: Erik Skultety <eskultet@redhat.com>

src/provisioner/machine.py
@@ -89,23 +89,22 @@ class Machine:
             try:
                 self.connect(ssh_key_path)
                 return
-            except (OSError, ssh_exception.NoValidConnectionsError) as ex:
-                if isinstance(ex, OSError):
-                    if ex.errno == EAI_NONAME:  # Name or service not known
-                        continue
-                    raise ex
+            except ssh_exception.NoValidConnectionsError as ex:
+                # NoValidConnectionsError is a subclass of various socket
+                # errors which in turn is a subclass of OSError. We're
+                # specifically interested in EHOSTUNREACH and ECONNREFUSED
+                # errnos which we can ignore for the duration of the timeout
+                # period
+                for error in ex.errors.values():
+                    if isinstance(error, OSError):
+                        if error.errno == errno.EHOSTUNREACH or error.errno == errno.ECONNREFUSED:
+                            break
                 else:
-                    # NoValidConnectionsError is a subclass of various socket
-                    # errors which in turn is a subclass of OSError. We're
-                    # specifically interested in EHOSTUNREACH and ECONNREFUSED
-                    # errnos which we can ignore for the duration of the timeout
-                    # period
-                    for error in ex.errors.values():
-                        if isinstance(error, OSError):
-                            if error.errno == errno.EHOSTUNREACH or error.errno == errno.ECONNREFUSED:
-                                break
-                    else:
-                        raise ex
+                    raise ex
+            except OSError as ex:
+                if ex.errno == EAI_NONAME:  # Name or service not known
+                    continue
+                raise ex
 
         raise Exception(f"Failed to connect to {self.name}: timeout reached")
 