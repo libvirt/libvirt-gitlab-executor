e2081a14cb41d519887a5ad69441904a28b43a4a
Erik Skultety <eskultet@redhat.com>
2023-09-25 15:37:22 +0200

    Adopt pyproject.toml build config
    Plain setup.py invocations are discouraged and pip tends to complain
    regularly about this. Therefore, adopt pyproject.toml build config
    template and use libvirt-ci.git's version as an inspiration.
    At the same time, edit setup.py so that it can be used in legacy setups
    and editable installs.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- setup.py
+++ setup.py
@@ -1,35 +1,9 @@
-import os
+# FIXME: Drop this file for good when setuptools v64.0.3 [1] becomes widely
+# available. We only need this file to support editable installs which
+# setuptools added in v64.0.0 by implementing PEP 660 [2]. It is recommended
+# to go with setuptools >v64.0.3 as it fixes some bugs in v64.0.0 in this area.
+# [1] https://setuptools.pypa.io/en/latest/history.html#v64-0-3
+# [2] https://peps.python.org/pep-0660/
+from setuptools import setup
 
-from setuptools import setup, Command
-
-
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        os.system("rm -vrf ./build ./dist ./*.pyc ./*.egg-info")
-
-
-setup(
-    name="libvirt-gci",
-    version="0.1",
-    packages=["provisioner"],
-    package_dir={"": "src"},
-    scripts=["bin/libvirt-gci"],
-    author="Erik Skultety",
-    author_email="eskultet@redhat.com",
-    description="GitLab CI libvirt VM provisioner",
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)"
-    ],
-    cmdclass={
-        "clean": CleanCommand,
-    }
-)
+setup()
